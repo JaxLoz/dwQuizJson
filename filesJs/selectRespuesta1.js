626fa2df5c3f5c9b534b7ceadd5bf80e15f05657
JavierMontes <jaxloz2002@gmail.com>
2023-01-23 00:10:29 -0500

    se agrego la funcionalidad de puntuacion para cada pregunta

---
+++ filesJs/selectRespuesta1.js
@@ -0,0 +1,23 @@
+let infoSesionCount = [];
+let SigPoint1 = document.getElementById("btn-firtsQ");
+SigPoint1.addEventListener("click", selectionRespuesta);
+
+function selectionRespuesta (){
+    let jsonBackInfoSesion = localStorage.getItem("registroSesionesLC");
+    let backInfoSesion = JSON.parse(jsonBackInfoSesion);
+    let objInfoSesion = backInfoSesion[0];
+    let checkBoxRespuestaCorrecta = document.getElementById("3-opcion-1").checked;
+
+    if(checkBoxRespuestaCorrecta === true){
+        objInfoSesion.sPoint += 10;
+        console.log("Selecciono la respuesta correcta para la primera pregunta y tiene en total "+objInfoSesion.sPoint);
+    }else{
+            console.log("selecciono la respuesta equivocada para la primera pregunta "+objInfoSesion.sPoint)
+    }
+
+    let indexDelete = backInfoSesion.findIndex(x => x.sEmail == objInfoSesion.sEmail);
+    backInfoSesion.splice(indexDelete,1);
+    backInfoSesion.unshift(objInfoSesion);
+    let jsonModifyInfoSesion = JSON.stringify(backInfoSesion);
+    localStorage.setItem("registroSesionesLC", jsonModifyInfoSesion);
+}
